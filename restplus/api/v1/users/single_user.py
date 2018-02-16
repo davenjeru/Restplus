11b3b9ae73c16ea19eb5ee725afd9498dcbf073f
Dave Mathews <davenmathews@gmail.com>
2018-02-16 18:28:59 +0300

    add functionality to get a single user

--- restplus/api/v1/users/single_user.py
+++ restplus/api/v1/users/single_user.py
@@ -1,11 +1,21 @@
 from flask_restplus import Resource
+from flask_restplus.namespace import Namespace
+
+from restplus.api.v1.helpers import safe_user_output
+from restplus.models import users_list
+
+users_ns = Namespace('users')
 
 
 class SingleUser(Resource):
     def get(self, user_id):
-        pass
+        for user in users_list:
+            if user.id == user_id:
+                return dict(user=safe_user_output(self, user))
+        else:
+            users_ns.abort(400, 'user not found!')
 
-    def put(self, user_id):
+    def patch(self, user_id):
         pass
 
     def delete(self, user_id):
