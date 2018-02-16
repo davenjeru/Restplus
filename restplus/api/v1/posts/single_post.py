6debd8ede858fb6f0843f54f1abea694bfc81b66
Dave Mathews <davenmathews@gmail.com>
2018-02-16 21:26:58 +0300

    reduce code duplication

--- restplus/api/v1/posts/single_post.py
+++ restplus/api/v1/posts/single_post.py
@@ -1,9 +1,12 @@
 from flask_restplus import Resource
 from flask_restplus.namespace import Namespace
 
+from restplus.api.v1.helpers import check_id_availability, safe_post_output
+from restplus.models import posts_list
+
 posts_ns = Namespace('posts')
 
 
 class SinglePost(Resource):
     def get(self, post_id):
-        pass
+        return dict(post=safe_post_output(self, check_id_availability(self, post_id, posts_list, 'post')))
--- restplus/api/v1/users/single_user_single_post.py
+++ restplus/api/v1/users/single_user_single_post.py
@@ -1,8 +1,10 @@
+from flask import redirect
 from flask_restplus import Resource
 from flask_restplus.namespace import Namespace
 
-from restplus.api.v1.helpers import check_id_availability, safe_post_output
-from restplus.models import users_list, posts_list
+from restplus.api.v1.helpers import check_id_availability
+from restplus.api.v1.posts.single_post import SinglePost
+from restplus.models import users_list
 
 users_ns = Namespace('users')
 
@@ -10,10 +12,7 @@ users_ns = Namespace('users')
 class SingleUserSinglePost(Resource):
     def get(self, user_id, post_id):
         check_id_availability(self, user_id, users_list, 'user')
-
-        a_post = check_id_availability(self, post_id, posts_list, 'post')
-
-        return dict(post=safe_post_output(self, a_post))
+        return redirect(self.api.url_for(SinglePost, post_id=post_id))
 
     def put(self, user_id, post_id):
         pass
