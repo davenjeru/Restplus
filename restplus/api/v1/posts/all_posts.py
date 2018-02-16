0c62743427d6f0e7d37880e17a02aad317d2a693
Dave Mathews <davenmathews@gmail.com>
2018-02-16 19:03:27 +0300

    add functionality to get all posts

--- restplus/api/v1/posts/all_posts.py
+++ restplus/api/v1/posts/all_posts.py
@@ -1,9 +1,15 @@
 from flask_restplus import Resource
 from flask_restplus.namespace import Namespace
 
+from restplus.api.v1.helpers import safe_post_output
+from restplus.models import posts_list
+
 posts_ns = Namespace('posts')
 
 
 class AllPosts(Resource):
     def get(self):
-        pass
+        posts = []
+        for post in posts_list:
+            posts.append(safe_post_output(self, post))
+        return dict(posts=posts)
